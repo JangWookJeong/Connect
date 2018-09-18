b29007ef1efb713f9adcc5cfd43e4ecca6eaf908
Krypto <codename444@naver.com>
2018-09-19 04:28:08 +0900

    18.09.18 수정본

--- app/src/main/java/project/codename/connect/Adapter/Mypage_content_RecyclerviewAdapter.java
+++ app/src/main/java/project/codename/connect/Adapter/Mypage_content_RecyclerviewAdapter.java
@@ -3,8 +3,6 @@ package project.codename.connect.Adapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class Mypage_content_RecyclerviewAdapter extends RecyclerView.ViewHolder<RecyclerView.ViewHolder> {
-    public Mypage_content_RecyclerviewAdapter(View itemView) {
-        super(itemView);
-    }
+public class Mypage_content_RecyclerviewAdapter  {
+
 }
