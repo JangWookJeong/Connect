0fa07fc478eeabb27f835cccfd15040cd132debe
Krypto <codename444@naver.com>
2018-09-24 01:47:09 +0900

    2018.09.24 업데이트

--- app/src/main/java/project/codename/connect/Activity/SignupProfileActivity.java
+++ app/src/main/java/project/codename/connect/Activity/SignupProfileActivity.java
@@ -1,34 +1,49 @@
 package project.codename.connect.Activity;
 
+import android.app.DatePickerDialog;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 import de.hdodenhof.circleimageview.CircleImageView;
-import project.codename.connect.Custom.Profile_Custom_Dialog;
+import project.codename.connect.Custom_Dialog.Profile_Custom_Dialog;
 import project.codename.connect.R;
 
 public class SignupProfileActivity extends AppCompatActivity {
     //프로필 설정 여부 확인 Profile_iscompleate
-    private Boolean Profile_iscompleate = false;
+
+    public static final int PROFILE_IMAGE_CODE = 200;
+    public static final int PROFILE_BACKGROUND_IMAGE_CODE = 201;
+    private Boolean Profile_iscompleate = false, Profile_Image_iscompleate = false;
     private TextView Set_Profile;
     private ImageButton Go_back;
-    private CircleImageView Profile_Image;
+    private CircleImageView Profile_Circle_Image;
     private TextView Nickname, Set_Nickname, Name, Set_Name;
     private RadioGroup Profile_RadioGroup;
     private String Profile_Gender_Result;
-    private TextView BirthDay,Set_BirthDay;
+    private TextView BirthDay, Set_BirthDay;
     private Button Register_Button;
     private Profile_Custom_Dialog dialog;
+    private DatePickerDialog dateDialog;
+    private DatePickerDialog.OnDateSetListener listener;
+    private String Profile_Image_Path, Profile_Background_Image_Path;
+    private ImageView Profile_Background_Image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +56,12 @@ public class SignupProfileActivity extends AppCompatActivity {
 
     private void createcomponent() {
 
-        dialog = new Profile_Custom_Dialog(getApplicationContext());
+        if (dialog == null) {
+            dialog = new Profile_Custom_Dialog(SignupProfileActivity.this);
+        }
         Go_back = findViewById(R.id.signup_profileactivity_imagebutton_backspacebutton);
         Set_Profile = findViewById(R.id.signup_profileactivity_textview_setprofile);
-        Profile_Image = findViewById(R.id.signup_profileactivity_circleimageview_profile_image);
+        Profile_Circle_Image = findViewById(R.id.signup_profileactivity_circleimageview_profile_image);
         Nickname = findViewById(R.id.signup_profileactivity_edittext_nickname);
         Set_Nickname = findViewById(R.id.signup_profileactivity_textview_setnickname);
         Name = findViewById(R.id.signup_profileactivity_textview_name);
@@ -53,6 +70,8 @@ public class SignupProfileActivity extends AppCompatActivity {
         BirthDay = findViewById(R.id.signup_profileactivity_textview_birthday);
         Set_BirthDay = findViewById(R.id.signup_profileactivity_textview_setbirthday);
         Register_Button = findViewById(R.id.signup_profileactivity_button_register);
+        Profile_Background_Image = findViewById(R.id.signup_profileactivity_Imageview_backgroundimage);
+
 
     }//////
 
@@ -65,19 +84,39 @@ public class SignupProfileActivity extends AppCompatActivity {
                 finish();
             }
         });/////Go_back
-        Profile_Image.setOnClickListener(new View.OnClickListener() {
+        Profile_Circle_Image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //프로필 이미지 클릭했을때 갤러리로 이동
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PROFILE_IMAGE_CODE);
+
             }
         });/////Profile_Image
+
+        Profile_Background_Image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PROFILE_BACKGROUND_IMAGE_CODE);
+            }
+        });
+
+        Set_Profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });/////
 
         Set_Nickname.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 //닉네임 설정
-                dialog.callFuntion(Nickname,"닉네임","닉네임을 설정하시면 다른사용자에게 닉네임으로 보여집니다.");
+                dialog.callFuntion(Nickname, "닉네임", "※ 닉네임을 설정하시면 다른사용자에게      닉네임으로 보여집니다.\r\n※ 부적합한 닉네임 사용시 이용제한됩니다.");
 
             }
         });/////Set_Nickname
@@ -87,7 +126,7 @@ public class SignupProfileActivity extends AppCompatActivity {
                 //이름 설정 클릭했을때
 
                 //닉네임 설정
-                dialog.callFuntion(Name,"이름","이름은 반드시 실명을 입력해주세요.");
+                dialog.callFuntion(Name, "이름", " ※ 이름이 실명과 다를경우 수익창출이 불가 합니다.");
             }
         });/////Set_Name
 
@@ -101,38 +140,100 @@ public class SignupProfileActivity extends AppCompatActivity {
         });/////Profile_RadioGroup
 
         Set_BirthDay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.callFuntion(Name,"생년월일","생년월일을 입력해주세요.");
-            }
-        });
 
-        Register_Button.setOnClickListener(new View.OnClickListener() {
+            //생년월일을 입력받을 데이트 피커 다이얼로그
             @Override
             public void onClick(View view) {
-                if(Nickname.getText().toString().length() > 0 && Nickname.getText() != null) {
-                    if(Name.getText().toString().length() > 0 && Name.getText() != null){
-                        if(Profile_Gender_Result.toString().length() > 0 && Profile_Gender_Result != null){
-                            if(BirthDay.getText().toString().length() > 0 && BirthDay.getText() != null){
+                if (dateDialog == null) {
+                    dateDialog = new DatePickerDialog(SignupProfileActivity.this, android.R.style.Theme_Holo_Dialog);
+                }
+                dateDialog.getDatePicker().setCalendarViewShown(false);
+                dateDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dateDialog.show();
+
+                dateDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        BirthDay.setText(year + "년" + (monthOfYear + 1) + "월" + dayOfMonth + "일");
+                    }
+                });
+
+            }
+
+
+        });/////
+
+        Register_Button.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View view) {
+                if (Nickname.getText().toString().length() > 0 && Nickname.getText() != null) {
+                    if (Name.getText().toString().length() > 0 && Name.getText() != null) {
+                        if (Profile_Gender_Result.toString().length() > 0 && Profile_Gender_Result != null) {
+                            if (BirthDay.getText().toString().length() > 0 && BirthDay.getText() != null) {
                                 Toast.makeText(SignupProfileActivity.this, Nickname + "닉네임" + Name + "이름" + Profile_Gender_Result + "성별" + BirthDay + "입니다.", Toast.LENGTH_SHORT).show();
-                            }else{
+                            } else {
                                 //생년월일을 입력하지 않은경우
                                 Toast.makeText(SignupProfileActivity.this, "생년월일을 설정해주세요.", Toast.LENGTH_SHORT).show();
                             }
 
-                        }else{
+                        } else {
                             //성별을 선택하지 않은경우
                             Toast.makeText(SignupProfileActivity.this, "성별을 설정해주세요.", Toast.LENGTH_SHORT).show();
                         }
-                    }else{
+                    } else {
                         //이름을 입력하지 않은경우
                         Toast.makeText(SignupProfileActivity.this, "이름을 설정해주세요.", Toast.LENGTH_SHORT).show();
                     }
-                }else{
+                } else {
                     //닉네임을 입력하지않은경우
                     Toast.makeText(SignupProfileActivity.this, "닉네임을 설정해주세요.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }/////addcomponent()
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PROFILE_IMAGE_CODE) {
+            if (data != null) {
+                //이미지 경로 얻어서 사진 설정
+                Profile_Image_Path = getPath(data.getData());
+                File file = new File(Profile_Image_Path);
+                Profile_Circle_Image.setImageURI(Uri.fromFile(file));
+                Profile_Image_iscompleate = true;
+
+            } else {
+                Toast.makeText(this, "이미지를 선택해주세요", Toast.LENGTH_SHORT).show();
+
+                return;
+            }//////PROFILE_IMAGE
+
+        }/////requestCode 확인
+
+        if (requestCode == PROFILE_BACKGROUND_IMAGE_CODE) {
+            if (data != null) {
+                Profile_Image_Path = getPath(data.getData());
+                File file = new File(Profile_Image_Path);
+                Profile_Background_Image.setImageURI(Uri.fromFile(file));
+
+            } else {
+                Toast.makeText(this, "배경 이미지를 선택해주세요", Toast.LENGTH_SHORT).show();
+            }
+        }/////PROFILE_BACKGROUND_IMAGE_CODE
+
+        Profile_Image_Path = "";
+    }/////onActivityResult
+
+    public String getPath(Uri uri) {
+
+        String[] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader = new CursorLoader(this, uri, proj, null, null, null);
+        Cursor cursor = cursorLoader.loadInBackground();
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(index);
+    }///////////////////////////getPath
 }/////class
--- app/src/main/java/project/codename/connect/Custom_Dialog/Profile_Custom_Dialog.java
+++ app/src/main/java/project/codename/connect/Custom_Dialog/Profile_Custom_Dialog.java
@@ -1,6 +1,5 @@
-package project.codename.connect.Custom;
+package project.codename.connect.Custom_Dialog;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
@@ -9,17 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import project.codename.connect.Activity.SignupProfileActivity;
 import project.codename.connect.R;
 
 public class Profile_Custom_Dialog {
 
     private Context Context;
-    private TextView Text_Title,Text_Explanation;
+    private TextView Text_Title, Text_Explanation;
     private Dialog dialog;
     private EditText Text_Content;
-    private Button Ok_Button,Cancel_Button;
-
+    private Button Ok_Button, Cancel_Button;
 
 
     public Profile_Custom_Dialog(Context context) {
@@ -27,14 +24,14 @@ public class Profile_Custom_Dialog {
 
     }//constructor
 
-    public void callFuntion(final TextView profile_text,String text,String annotation){
-         dialog = new Dialog(Context);
+    public void callFuntion(final TextView profile_text, String text, String annotation) {
+        dialog = new Dialog(Context);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.profile_custom_dialog);
         createponent();
-        addcomponent(text,annotation);
+        addcomponent(text, annotation);
         addlistener(profile_text);
-        dialog.show();
+
 
     }/////callFuntion
 
@@ -54,16 +51,18 @@ public class Profile_Custom_Dialog {
         });
     }
 
-
     private void createponent() {
 
+        dialog.show();
         Text_Title = (TextView) dialog.findViewById(R.id.profile_custom_dialog_textview_title);
         Text_Explanation = (TextView) dialog.findViewById(R.id.profile_custom_dialog_textview_explanation);
         Text_Content = (EditText) dialog.findViewById(R.id.profile_custom_dialog_edittext);
         Ok_Button = (Button) dialog.findViewById(R.id.profile_custom_dialog_okbutton);
-        Cancel_Button = (Button)dialog.findViewById(R.id.profile_custom_dialog_cancelbutton);
+        Cancel_Button = (Button) dialog.findViewById(R.id.profile_custom_dialog_cancelbutton);
 
-    };
+    }
+
+    ;
 
     private void addcomponent(String text, String annotation) {
         Text_Title.setText(text);
